5dd9ef5c691cb8592535a340ea65f2eafd20a724
mariamistica002 <mariamistica002@gmail.com>
2026-05-29 16:17:52 +0530

    fix attendance migration dependency

--- backend/attendance/migrations/0001_initial.py
+++ backend/attendance/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('courses', '__first__'),
+        ('courses', '0001_initial'),
     ]
 
     operations = [
